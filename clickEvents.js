ebe21189f330441d8d460d24961eea2b9d67a9d6
Matthew <mfarmer5102@gmail.com>
2019-07-20 19:06:31 -0400

    Added alerts to notify visitors when an app cannot be experienced

---
+++ clickEvents.js
@@ -0,0 +1,9 @@
+//#10 - LIRI Bot
+$(document).on("click", "#liriBotThumbnail", function () {
+    alert('Unfortunately this application is unavailable to experience here as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or demonstrative recordings.')
+});
+
+//#11 - Hangman
+$(document).on("click", "#hangmanNodeThumbnail", function () {
+    alert('Unfortunately this application is unavailable to experience here as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or demonstrative recordings.')
+});
