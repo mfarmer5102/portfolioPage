1b56b7a64421a90a525e178a3929b4e466702399
Matthew <mfarmer5102@gmail.com>
2019-07-22 20:33:35 -0400

    Added study helper to portfolio

--- clickEvents.js
+++ clickEvents.js
@@ -1,9 +1,14 @@
 //#10 - LIRI Bot
 $(document).on("click", "#liriBotThumbnail", function () {
-    alert('Unfortunately this application is unavailable to experience here as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or demonstrative recordings.')
+    alert('Unfortunately this application is unavailable to experience as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or a demonstrative recording.')
 });
 
 //#11 - Hangman
 $(document).on("click", "#hangmanNodeThumbnail", function () {
-    alert('Unfortunately this application is unavailable to experience here as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or demonstrative recordings.')
+    alert('Unfortunately this application is unavailable to experience as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or a demonstrative recording.')
+});
+
+//Study Helper
+$(document).on("click", "#studyHelperThumbnail", function () {
+    alert('Unfortunately this application is unavailable to experience as its MySQL database is not remotely hosted. You may still view its source code on GitHub along with screenshots and/or a demonstrative recording.')
 });
