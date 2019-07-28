ce292b4c8a0fa428b9d759113b6dc12124c79167
Matthew <mfarmer5102@gmail.com>
2019-07-27 21:14:58 -0400

    Now collecting traffic information on traffic to specific featured apps

--- trafficMonitor.js
+++ trafficMonitor.js
@@ -15,15 +15,33 @@ var database = firebase.database();
 ///////////// VARIABLE INITIALIZATION //////////////////////////////
 
 let accrEntryPointButton = 0;
+let accrDivStudyHelper = 0;
+let accrDivDashboard = 0;
 
 ///////////// INITIAL AND CONTINUOUS DB SYNCING //////////////////////////////
 
 database.ref().on("value", function (snapshot) {
     console.log(snapshot.val());
-    if (snapshot.val() === null ) {
+    if (snapshot.val() === null) {
         accrEntryPointButton = 0;
+        accrDivStudyHelper = 0;
+        accrDivDashboard = 0;
     } else {
-        accrEntryPointButton = snapshot.val().logs.entryPointButton.total
+        if (snapshot.val().logs.entryPointButton !== undefined) {
+            accrEntryPointButton = snapshot.val().logs.entryPointButton.total
+        } else {
+            accrEntryPointButton = 0;
+        }
+        if (snapshot.val().logs.divStudyHelper !== undefined) {
+            accrDivStudyHelper = snapshot.val().logs.divStudyHelper.total
+        } else {
+            accrDivStudyHelper = 0;
+        }
+        if (snapshot.val().logs.divDashboard !== undefined) {
+            accrDivDashboard = snapshot.val().logs.divDashboard.total
+        } else {
+            accrDivDashboard = 0;
+        }
     }
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
@@ -36,6 +54,24 @@ $(document).on("click", "#entryPointButton", function () {
     let clickRecord = `logs/entryPointButton/${accrEntryPointButton + 1}/timeStamp`
     database.ref('/').update({
         [accumulator]: accrEntryPointButton + 1,
+        [clickRecord]: firebase.database.ServerValue.TIMESTAMP
+    });
+});
+
+$(document).on("click", "#divStudyHelper", function () {
+    let accumulator = `logs/divStudyHelper/total`
+    let clickRecord = `logs/divStudyHelper/${accrDivStudyHelper + 1}/timeStamp`
+    database.ref('/').update({
+        [accumulator]: accrDivStudyHelper + 1,
+        [clickRecord]: firebase.database.ServerValue.TIMESTAMP
+    });
+});
+
+$(document).on("click", "#divDashboard", function () {
+    let accumulator = `logs/divDashboard/total`
+    let clickRecord = `logs/divDashboard/${accrDivDashboard + 1}/timeStamp`
+    database.ref('/').update({
+        [accumulator]: accrDivDashboard + 1,
         [clickRecord]: firebase.database.ServerValue.TIMESTAMP
     });
 });
