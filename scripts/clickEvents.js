036d47494eeb02fc96750c89cb263f49609eb2fe
Matthew <mfarmer5102@gmail.com>
2019-07-29 22:44:21 -0400

    Added the bamazon project to applications page

--- scripts/clickEvents.js
+++ scripts/clickEvents.js
@@ -10,6 +10,11 @@ $(document).on("click", "#hangmanNodeThumbnail", function () {
     alert('Unfortunately this application is unavailable to experience as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or a demonstrative recording.')
 });
 
+//#12 - Bamazon
+$(document).on("click", "#bamazonThumbnail", function () {
+    alert('Unfortunately this application is unavailable to experience as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or a demonstrative recording.')
+});
+
 //Study Helper
 $(document).on("click", "#studyHelperThumbnail", function () {
     alert('Unfortunately this application is unavailable to experience as its MySQL database is not remotely hosted. You may still view its source code on GitHub along with screenshots and/or a demonstrative recording.')
