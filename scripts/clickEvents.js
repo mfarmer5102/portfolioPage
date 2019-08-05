6b28faa4e47d7f0a8b84a0da913dcd8f1a19ba13
Matthew <mfarmer5102@gmail.com>
2019-08-05 16:33:43 -0400

    Added a nonJS app section and added the python financial reporter

--- scripts/clickEvents.js
+++ scripts/clickEvents.js
@@ -14,3 +14,8 @@ $(document).on("click", "#hangmanNodeThumbnail", function () {
 $(document).on("click", "#bamazonThumbnail", function () {
     alert('Unfortunately this application is unavailable to experience as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or a demonstrative recording.')
 });
+
+//Python Financial Reporter
+$(document).on("click", "#pyFinRepThumbnail", function () {
+    alert('Unfortunately this application is unavailable to experience as it is a command-line application. You may still view its source code on GitHub along with screenshots and/or a demonstrative recording.')
+});
